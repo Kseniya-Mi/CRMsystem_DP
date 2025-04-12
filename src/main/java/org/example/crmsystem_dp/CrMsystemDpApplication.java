95a704124a4c097e0d81cb253759af51364cdd80
Kseniya <miron2014857@gmail.com>
2025-04-12 15:26:15 +0300

    Initial commit

---
+++ src/main/java/org/example/crmsystem_dp/CrMsystemDpApplication.java
@@ -0,0 +1,13 @@
+package org.example.crmsystem_dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrMsystemDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrMsystemDpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/crmsystem_dp/CrMsystemDpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crmsystem_dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrMsystemDpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
